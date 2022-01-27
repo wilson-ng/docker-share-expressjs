7ead051ec870ab56c93cc109e4041df0985d169e
Wilson <wilson.ng@finaccel.co>
2022-01-27 15:01:39 +0700

    adding container id

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let containerId;
+const { exec } = require("child_process");
+exec("head -1 /proc/self/cgroup|cut -d/ -f5", (error, stdout, stderr) => {containerId = stdout});
+
 app.get('/', (req, res) => {
-    res.send('Hi everyone, thanks for joining docker --share, hosted on container.');
+    res.status(200).json({
+        greet: `Hi everyone, thanks for joining docker --share.`,
+        container_id: containerId,
+    });
 });
 
 app.listen(port, () => {
